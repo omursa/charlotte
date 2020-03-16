7860ed9e53dfdabb911db60d08b81691506d1869
omursa <omursahin43@gmail.com>
2020-03-16 13:56:31 -0400

    first comment

---
+++ src/interview/ArmStrongNumbers.java
@@ -0,0 +1,38 @@
+package interview;
+
+public class ArmStrongNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<=1000; i++) {
+			
+			if(isArm(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static boolean isArm(int num) {
+		boolean flag= false;
+		
+		int sum=num;
+		int r= 1;
+		int cube= 0;
+		
+		
+		
+		while(num>0) {
+			r= num %10; 
+			num=num/10;
+			cube= cube+ r*r*r;
+			
+		}
+		
+		if(sum==cube) {
+			flag= true;
+		}
+		return flag;
+		
+	
+	}
+}
